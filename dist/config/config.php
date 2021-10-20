f5427be5aec471b19e296615a9c544bdf2bdf8f1
kitahamad <d.kitahama@grow-group.jp>
2021-10-20 11:01:39 +0900

    form

--- dist/config/config.php
+++ dist/config/config.php
@@ -7,20 +7,24 @@
 $config['to_email'] = 'webmaster@grow-group.jp';
 
 // 管理者宛_メール件名
-$config['to_subject'] = '［株式会社サンプル］お問い合わせが届きました';
+$config['to_subject'] = '［株式会社地域活性プランニング］お問い合わせが届きました';
 
 // 自動返信を有効に
 $config['auto_reply'] = true;
 
 // 自動返信メール_件名
-$config['auto_reply_subject'] = '［株式会社サンプル］お問い合わせありがとうございます';
+$config['auto_reply_subject'] = '［株式会社地域活性プランニング］お問い合わせありがとうございます';
 
 // 自動返信メール_送信者名
-$config['auto_reply_name'] = '株式会社サンプル';
+$config['auto_reply_name'] = '株式会社地域活性プランニング';
 
 // 自動返信メール_ユーザー入力メールアドレスの input name 値
-$config['auto_reply_email'] = 'E-mail';
+$config['auto_reply_email'] = 'メールアドレス';
 
+$config['file'] = true;
+$config['file_allow_extension'] = "jpg,jpeg,gif,png,pdf,xlsx,docx,doc,xls";
+$config['file_max_size'] = 10120000;
+$config['file_retention_period'] = 1800;
 
 // ## SMTP設定 ##
 // $config['smtp'] = true;
--- dist/index.php
+++ dist/index.php
@@ -11,6 +11,5 @@ require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . '/lib/TransmitMail.php';
 $tm = new TransmitMail('config/config.php');
 echo $tm->tpl->fetch("./templates/header.html");
-echo $tm->tpl->fetch("./templates/main.html");
 $tm->run();
 echo $tm->tpl->fetch("./templates/footer.html");
